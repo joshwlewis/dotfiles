ab5ae29ecc7dc7e074bed9bb5e67ca003bc1e802
Josh W Lewis <josh.w.lewis@gmail.com>
2016-08-19 21:23:42 -0500

    Add hyperterm config

---
+++ .hyperterm.js
@@ -0,0 +1,50 @@
+module.exports = {
+  config: {
+    fontSize: 13,
+
+    fontFamily: '"Source Code Pro for Powerline", "Source Code Pro", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+
+    cursorColor: 'rgba(248,28,229,0.75)',
+
+    cursorShape: 'BLOCK',
+
+    foregroundColor: '#7f7a7e',
+
+    backgroundColor: '#fff',
+
+    borderColor: '#bbb',
+
+    css: `
+      .tabs_title {
+        color: #625b60;
+      }
+    `,
+
+    termCSS: '',
+
+    padding: '8px 12px',
+
+    colors: {
+      black: '#41383f',
+      red: '#e23391',
+      green: '#03b9a9',
+      yellow: '#e49b0c',
+      blue: '#8776f4',
+      magenta: '#b352e3',
+      cyan: '#02ace1',
+      white: '#cfcdce',
+      lightBlack: '#9b979a',
+      lightRed: '#e06901',
+      lightGreen: '#625b60',
+      lightYellow: '#7f7a7e',
+      lightBlue: '#b6b2b5',
+      lightMagenta: '#e7e6e7',
+      lightCyan: '#d443d4',
+      lightWhite: '#ffffff'
+    },
+  },
+
+  plugins: [],
+
+  localPlugins: []
+};
