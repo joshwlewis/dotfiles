dbfd33be0bc813b63a89444d4084e187db31aa59
Josh W Lewis <josh.w.lewis@gmail.com>
2025-01-07 23:06:48 -0600

    Switch to kitty and lazy.nvim

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,51 +1,57 @@
 #!/usr/bin/env bash
 
-script_dir="$(cd "$(dirname "$0")" ; pwd -P)"
+script_dir="$(
+    cd "$(dirname "$0")"
+    pwd -P
+)"
 
 dotfiles=(
-  .rgignore
-  .aliases
-  .alacritty.toml
-  .asdfrc
-  .bash_profile
-  .bash_prompt
-  .bashrc
-  .curlrc
-  .editorconfig
-  .exports
-  .functions
-  .gdbinit
-  .gemrc
-  .gitattributes
-  .gitconfig
-  .gvimrc
-  .hgignore
-  .hushlogin
-  .hyperterm.js
-  .inputrc
-  .irbrc
-  .screenrc
-  .tmux.conf
-  .vimrc
-  .wgetrc
-  .zshrc
+    .config/kitty/kitty.conf
+    .config/nvim/init.lua
+    .rgignore
+    .aliases
+    .alacritty.toml
+    .asdfrc
+    .bash_profile
+    .bash_prompt
+    .bashrc
+    .curlrc
+    .editorconfig
+    .exports
+    .functions
+    .gdbinit
+    .gemrc
+    .gitattributes
+    .gitconfig
+    .gvimrc
+    .hgignore
+    .hushlogin
+    .hyperterm.js
+    .inputrc
+    .irbrc
+    .screenrc
+    .tmux.conf
+    .vimrc
+    .wgetrc
+    .zshrc
 )
 
 function symlinkAll() {
-  for f in "${dotfiles[@]}"
-  do
-    echo $script_dir/$f
-    rm ~/$f; ln -s $script_dir/$f ~/$f
-  done
+    for f in "${dotfiles[@]}"; do
+        echo $script_dir/$f
+        mkdir -p "~$(dirname "$f")"
+        rm ~/$f
+        ln -s $script_dir/$f ~/$f
+    done
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  symlinkAll;
+    symlinkAll
 else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-  echo "";
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    symlinkAll;
-  fi;
-fi;
-unset symlinkAll;
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        symlinkAll
+    fi
+fi
+unset symlinkAll
